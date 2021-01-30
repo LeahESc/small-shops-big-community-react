9465f31074d4645e979e5eca8dc9c18032da8d64
LeahESc <leahemily1@gmail.com>
2021-01-30 11:30:50 -0800

    properly exports Shops component

---
+++ src/components/Shops.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Shops extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
