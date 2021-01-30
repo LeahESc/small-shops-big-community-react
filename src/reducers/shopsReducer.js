935ded5317c8c815a8d3e0cc19758a33f4b47a26
LeahESc <leahemily1@gmail.com>
2021-01-30 10:44:55 -0800

    fixes reducer function again, checks for proper tags

--- src/reducers/shopsReducer.js
+++ src/reducers/shopsReducer.js
@@ -8,12 +8,12 @@ const shopsReducer = (state= {shops: [], loading: false}, action) => {
 
         case 'SHOPS_LOADED':
             console.log(action)
-            let shops = action.shops
             const tagOptions = action.tags 
-            // this filter method works for now but only matches the first tag to the first tag in the selected parameters
-            shops.filter(s => { 
-                if (s.tags.some(t => t.id === tagOptions[0].id)) {
-                    return s } })
+            const shops = action.shops.filter(s => { 
+                            if (s.tags.some(t => t.id === tagOptions[0].id)) {
+                                return s 
+                            } 
+                        })
             console.log("filtered shops:", shops)
             return {
                 ...state,
