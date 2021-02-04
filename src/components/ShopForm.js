38b25ee38d8bf3fe6047eaf3a6cc73ce1250daea
LeahESc <leahemily1@gmail.com>
2021-02-04 08:50:36 -0800

    adds and styles a new shop form

--- src/components/ShopForm.js
+++ src/components/ShopForm.js
@@ -1,30 +1,105 @@
 import React, { Component } from 'react'
 import { Form, Button } from 'semantic-ui-react'
+import TagCheckbox from './TagCheckbox'
 
 export default class ShopForm extends Component {
+    state = {
+        name: '',
+        category_id: this.props.category.id,
+        description: '',
+        social_impact: '',
+        address: '',
+        website: '',
+        tags: [
+            {id: 1, value:"BIPOC-OWNED", checked: false},
+            {id: 2, value:"WOMEN/WOMXN-OWNED", checked: false},
+            {id: 3, value:"LGBTQ+-OWNED", checked: false},
+            {id: 4, value:"SOCIAL IMPACT COMMITMENT", checked: false}
+        ],
+    }
+
+    createTagCheckboxes = () =>  this.state.tags.map(tag => <div className='tag-checkbox'> <TagCheckbox key={tag.id} value={tag.value} checked={tag.checked} handleCheck={this.handleCheck} /> </div>)
+
+
+    handleCheck = (e) => {
+        const name = e.target.name;
+        this.setState({  
+            ...this.state,
+            tags: [
+            ...this.state.tags.map(tag => {
+                if (tag.value === name ) {
+                    return {...tag, checked: !tag.checked}
+                } else {
+                    return tag
+                }
+            })]
+        })
+    };
+
+    handleChange = (e) => { 
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value
+        })    
+    }
+
     render() {
         return (
-            
+            <div className='new-shop-form'>
+                <h4>Don't see the business you're looking for in this search? Add it!</h4>
             <Form>
                  <Form.Input
                     // error={{ content: 'Please enter your first name', pointing: 'below' }}
                     fluid
-                    label='First name'
-                    placeholder='First name'
+                    label='Shop Name'
+                    name='name'
+                    placeholder=''
                     id='form-input-first-name'
+                    onChange={this.handleChange}
+                    value={this.state.name}
                 />
-                <Form.Field>
-                    <label>Address</label>
-                    <input placeholder='' />
-                </Form.Field>
-                <Form.Field>
-                    <label>Description</label>
-                    <input placeholder='Type up a few words about this shop' />
-                </Form.Field>
-                <Form.Field>
-                </Form.Field>
+                <Form.Input
+                    // error={{ content: 'Please enter your first name', pointing: 'below' }}
+                    fluid
+                    label='Shop Description'
+                    name='description'
+                    placeholder=''
+                    id='form-input-shop-description'
+                    onChange={this.handleChange}
+                    value={this.state.description}
+                />
+                <Form.Input
+                    // error={{ content: 'Please enter your first name', pointing: 'below' }}
+                    fluid
+                    label='Social Impact (if any)'
+                    name='social_impact'
+                    placeholder=''
+                    id='form-input-shop-impact'
+                    onChange={this.handleChange}
+                    value={this.state.social_impact}
+                />
+                <Form.Input
+                    // error={{ content: 'Please enter your first name', pointing: 'below' }}
+                    label='Address'
+                    name='address'
+                    placeholder=''
+                    id='form-input-shop-impact'
+                    onChange={this.handleChange}
+                    value={this.state.address}
+                />
+                <Form.Input
+                    // error={{ content: 'Please enter your first name', pointing: 'below' }}
+                    label='Website'
+                    name='website'
+                    placeholder=''
+                    id='form-input-shop-impact'
+                    onChange={this.handleChange}
+                    value={this.state.website}
+                />
+                {this.createTagCheckboxes()}
                 <Button type='submit'>Submit</Button>
             </Form>
+            </div>
         )
        
     }
