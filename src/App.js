95fe682b62e92159e280aac7fd9fca05e29331b7
LeahESc <leahemily1@gmail.com>
2021-04-11 19:13:15 -0400

    refactors using useSelector hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { useEffect } from 'react'
 import HomeSearch from './containers/HomeSearch'
-import fetchCategories from './actions/categoryActions'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import CategoryContainer from './containers/CategoryContainer'
 import ShopContainer from './containers/ShopContainer'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
+import fetchCategories from './actions/categoryActions'
 
   
 const App = () => {  
   const dispatch = useDispatch()
 
+
   useEffect( () => { 
       dispatch(fetchCategories())
   })
-
+  
     return (
       <Router>
         <div className="App">
@@ -29,6 +30,7 @@ const App = () => {
 
 export default App;
  
+
 
 //  I used to pass fetchCategories={props.fetchCategories}  to HomeSearch component but I no longer need to now that I'm using Redux hooks (I think?)
 //  no longer need to use connect(null ,{ fetchCategories })
--- src/containers/ShopContainer.js
+++ src/containers/ShopContainer.js
@@ -1,56 +1,51 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReviewForm from '../components/ReviewForm'
 import Reviews from '../components/Reviews'
 import Shop from '../components/Shop'
 import ShopCarousel from '../components/ShopCarousel'
 import ShopMap from '../components/ShopMap'
 import { Grid, Item } from 'semantic-ui-react'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import uuid from 'react-uuid'
 
-class ShopContainer extends Component {
+const ShopContainer = (props) => {
+        
+    const categories = useSelector(state => state.categories)
+    
+    const urlArray = props.match.url.split('/')
+    const categoryId = parseInt(urlArray[2])
+    const shopId = parseInt(urlArray[urlArray.length-1])
+    const category = categories.find(category => category.id === categoryId)
+    const shop = category.shops.find(shop => shop.id === shopId)
 
-    render() {
-        const urlArray = this.props.match.url.split('/')
-        const categoryId = parseInt(urlArray[2])
-        const shopId = parseInt(urlArray[urlArray.length-1])
-        const category = this.props.categories.find(category => category.id === categoryId)
-        const shop = category.shops.find(shop => shop.id === shopId)
+    return (
+        <div>
+        <Item key={uuid()} >
+            <Item.Content key={uuid()} >
+                <ShopCarousel key={uuid()}  shop={shop} />
+            </Item.Content>
+        </Item >
+        <Grid key={uuid()} columns={3} >
+            <Grid.Column  key={uuid()} width={10}>
+                <Item.Group key={uuid()} divided>
+                    <Item key={uuid()} >
+                    <br/>
+                        <Item.Content key={uuid()} >
+                            <Shop key={uuid()} url={this.props.match.url} category_id={categoryId} name={shop.name} id={shop.id} description={shop.description} social_impact={shop.social_impact} address={shop.address} website={shop.website} tags={shop.tags}/> 
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+                <ReviewForm key={uuid()}shop={shop} category={category}/>
+                <Reviews key={uuid()} shop={shop}  />
+            </Grid.Column>
+            
+            <Grid.Column key={uuid()}width={4} floated='left'>
+                <ShopMap key={uuid()}shop={shop} />
+            </Grid.Column>
+            <Grid.Column key={uuid()}width={2}></Grid.Column>
+        </Grid>
+        </div>
+    )
+}
 
-        return (
-            <div>
-            <Item key={uuid()} >
-                <Item.Content key={uuid()} >
-                    <ShopCarousel key={uuid()}  shop={shop} />
-                </Item.Content>
-            </Item >
-            <Grid key={uuid()} columns={3} >
-                <Grid.Column  key={uuid()} width={10}>
-                    <Item.Group key={uuid()} divided>
-                        <Item key={uuid()} >
-                        <br/>
-                            <Item.Content key={uuid()} >
-                                <Shop key={uuid()} url={this.props.match.url} category_id={categoryId} name={shop.name} id={shop.id} description={shop.description} social_impact={shop.social_impact} address={shop.address} website={shop.website} tags={shop.tags}/> 
-                            </Item.Content>
-                        </Item>
-                    </Item.Group>
-                    <ReviewForm key={uuid()}shop={shop} category={category}/>
-                    <Reviews key={uuid()} shop={shop}  />
-                </Grid.Column>
-             
-                <Grid.Column key={uuid()}width={4} floated='left'>
-                    <ShopMap key={uuid()}shop={shop} />
-                </Grid.Column>
-                <Grid.Column key={uuid()}width={2}></Grid.Column>
-            </Grid>
-         </div>
-        )
-    }
-}
-      
-const mapStateToProps = state => { 
-    return { 
-        categories: state.categoriesReducer.categories
-    }
-}
-export default connect(mapStateToProps)(ShopContainer)
+export default ShopContainer 
