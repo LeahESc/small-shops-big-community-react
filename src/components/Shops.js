6aa03bab866461d432835cf9c4647e94e017efcb
LeahESc <leahemily1@gmail.com>
2021-04-12 17:46:02 -0400

    fixes shops component props;

--- src/components/Shops.js
+++ src/components/Shops.js
@@ -4,17 +4,25 @@ import Shop from './Shop'
 import { Item } from 'semantic-ui-react'
 
 
-const Shops = () => { 
+const Shops = (props) => { 
     return (
         <>
-        {this.props.shops.map(shop => ( 
+        {props.shops.map(shop => ( 
 
         <Item.Group divided>
             <Item>
                 <Item.Image src={shopicon} />
 
                 <Item.Content>
-                    <Shop key={shop.id} url={this.props.url} category_id={shop.category_id} name={shop.name} id={shop.id} description={shop.description} social_impact={shop.social_impact} address={shop.address} website={shop.website} tags={shop.tags}/> 
+                    <Shop key={shop.id} 
+                        url={props.url} 
+                        category_id={shop.category_id} 
+                        name={shop.name} id={shop.id} 
+                        description={shop.description} 
+                        social_impact={shop.social_impact} 
+                        address={shop.address} 
+                        website={shop.website} 
+                        tags={shop.tags}/> 
                 </Item.Content>
             </Item>
         </Item.Group>
