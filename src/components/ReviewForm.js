7dd0012bf4e0b87c90512469940fae54655de783
LeahESc <leahemily1@gmail.com>
2021-04-12 17:37:40 -0400

    converts review form to functional component with hooks

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,41 +1,42 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import {Form, TextArea, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import {addReview} from '../actions/reviewActions'
 
-class ReviewForm extends Component {
+// class ReviewForm extends Component {
+const ReviewForm = () => { 
     
+    const dispatch = useDispatch()
+    const categories = useSelector(state => state.categories)
+    const [ text, setText ] = useState('')
     state= {
         text: ''
     }
     
     handleChange = (e) => {
-        this.setState({
-            text: e.target.value
-        })
+      
     }
-
+    
     handleSubmit = (e) => {
         e.preventDefault()
-        if (this.state.text === '') {
+        if (text === '') {
             alert('Please write a review before hitting submit')
         } else { 
         const category = this.props.category
         const newReview = {...this.state, shop_id: this.props.shop.id}
         console.log("review obj:", newReview)
-        this.props.addReview(newReview, category)
-        this.setState({
-            text: ''
-        })}
+        dispatch(addReview(newReview, category))
+        setText(' ')
     }
 
-    render() {
+    // render() {
         return (
             <div className='review-form'>
                 <p className='review-text'>Visited this shop recently? What did you purchase that you love? Leave a review below!</p> 
             
                 <Form onSubmit={this.handleSubmit}> 
-                    <TextArea id={this.props.shop.id} onChange={this.handleChange} value={this.state.text} placeholder="The owners here are incredible!" />
+                    <TextArea id={this.props.shop.id} onChange={e => setText(e.target.value)} value={text} placeholder="The owners here are incredible!" />
                     <Button color="yellow" type="submit">
                         Submit
                     </Button>
@@ -45,11 +46,11 @@ class ReviewForm extends Component {
             </div>
         )
     }
-}
 
-const mapStateToProps = state => { 
-    return { 
-        categories: state.categoriesReducer.categories, 
-    }
-}
+
+// const mapStateToProps = state => { 
+//     return { 
+//         categories: state.categoriesReducer.categories, 
+//     }
+// }
 export default connect(mapStateToProps, {addReview})(ReviewForm)
