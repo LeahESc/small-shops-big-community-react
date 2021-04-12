51aaed29f70b1ec690a6cf431f564677d3fcba4a
LeahESc <leahemily1@gmail.com>
2021-04-12 11:07:23 -0400

    adds useeffect hook to mapcontainer

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -3,6 +3,8 @@ import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import mapStyles from '../mapStyles'
 import shopicon from '../images/shopicon.jpg'
 import uuid from 'react-uuid'
+import { useEffect } from 'react'
+
 
 const mapContainerStyle = {
     height: "90vh",
@@ -21,22 +23,8 @@ const options = {
 
 const MapContainer = (props) =>  {
     
-    // const [selectedShop, setSelectedShop] = useState(null)
-
-    const {isLoaded, loadError} = useLoadScript({
-        googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY 
-    })
-
-    if (loadError) return "There was an error loading Google Maps"
-    if(!isLoaded) return "Loading your map!"
-    
-    // const defaultLocation = {
-    //     lat: 34.0522, 
-    //     lng: -118.2437
-    // }
-
-    useEffect((props) => {
-        let geocodeLocations = props.shops.map(shop => {
+    useEffect(() => {
+        return geocodeLocations = props.shops.map(shop => {
             let location = shop.address.split(' ').join('+')
         // let key = process.env.REACT_APP_GOOGLE_API_KEY
         fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${key}`, {
@@ -52,6 +40,22 @@ const MapContainer = (props) =>  {
         console.log("returned location object:", geocodeLocations)
         })
     })
+    // const [selectedShop, setSelectedShop] = useState(null)
+
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY 
+    })
+
+    if (loadError) return "There was an error loading Google Maps"
+    if(!isLoaded) return "Loading your map!"
+    
+    // const defaultLocation = {
+    //     lat: 34.0522, 
+    //     lng: -118.2437
+    // }
+    let geocodedLocations 
+
+   
 
    
 
@@ -62,12 +66,11 @@ const MapContainer = (props) =>  {
         center={center}
         options={options}>
 
-        {props.shops.map(shop => {
-            let gc = geocode(shop)
+        {geocodedLocations.map(shop => {
             return ( 
             <Marker 
                 key={uuid()} 
-                position={gc}
+                position={shop}
                 // onClick={() => setSelectedShop(shop)}
                 icons={{
                     icon: shopicon,
@@ -89,24 +92,24 @@ const MapContainer = (props) =>  {
 }
 
  
-const geocode = (shop) => {
-    let location = shop.address.split(' ').join('+')
-    let key = process.env.REACT_APP_GOOGLE_API_KEY
-    let locationObject
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${key}`, {
-        params: {
-            address: shop.address,
-            key: process.env.REACT_APP_GOOGLE_API_KEY
-        }
-    })
-    .then(resp => resp.json())
-    .then(data => {
-        console.log(data.results[0].geometry.location)
-        let locationObject = data.results[0].geometry.location
-        console.log("loc obj", locationObject)
-        return locationObject
-    })
-    console.log("returned location object:", locationObject)
-}
+// const geocode = (shop) => {
+//     let location = shop.address.split(' ').join('+')
+//     let key = process.env.REACT_APP_GOOGLE_API_KEY
+//     let locationObject
+//     fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${key}`, {
+//         params: {
+//             address: shop.address,
+//             key: process.env.REACT_APP_GOOGLE_API_KEY
+//         }
+//     })
+//     .then(resp => resp.json())
+//     .then(data => {
+//         console.log(data.results[0].geometry.location)
+//         let locationObject = data.results[0].geometry.location
+//         console.log("loc obj", locationObject)
+//         return locationObject
+//     })
+//     console.log("returned location object:", locationObject)
+// }
 
 export default MapContainer
