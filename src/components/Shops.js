d3d578017f849633fa2658760853d40ecb27eb3a
LeahESc <leahemily1@gmail.com>
2021-04-12 17:22:18 -0400

    converts Shops class component to functional component

--- src/components/Shops.js
+++ src/components/Shops.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import shopicon from '../images/shopicon.jpg'
 import Shop from './Shop'
 import { Item } from 'semantic-ui-react'
 
-export default class Shops extends Component {
-    render() {
-        
-        return (
-            <>
-            {this.props.shops.map(shop => ( 
 
-            <Item.Group divided>
-                <Item>
-                    <Item.Image src={shopicon} />
+const Shops = () => { 
+    return (
+        <>
+        {this.props.shops.map(shop => ( 
 
-                    <Item.Content>
-                        <Shop key={shop.id} url={this.props.url} category_id={shop.category_id} name={shop.name} id={shop.id} description={shop.description} social_impact={shop.social_impact} address={shop.address} website={shop.website} tags={shop.tags}/> 
-                    </Item.Content>
-                </Item>
-            </Item.Group>
-            ))}
-            </>
-        )
-    }
+        <Item.Group divided>
+            <Item>
+                <Item.Image src={shopicon} />
+
+                <Item.Content>
+                    <Shop key={shop.id} url={this.props.url} category_id={shop.category_id} name={shop.name} id={shop.id} description={shop.description} social_impact={shop.social_impact} address={shop.address} website={shop.website} tags={shop.tags}/> 
+                </Item.Content>
+            </Item>
+        </Item.Group>
+        ))}
+        </>
+    )
 }
+export default Shops;
