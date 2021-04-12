0a5a5ea6c84e6e2954227a23dd5fa715dfa1a44d
LeahESc <leahemily1@gmail.com>
2021-04-12 17:44:23 -0400

    fixes compiling issue with export of reviewform

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,51 +1,55 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import {Form, TextArea, Button } from 'semantic-ui-react'
-import { connect } from 'react-redux'
+// import { connect } from 'react-redux'
 import {addReview} from '../actions/reviewActions'
 
 // class ReviewForm extends Component {
-const ReviewForm = () => { 
+const ReviewForm = (props) => { 
     
     const dispatch = useDispatch()
     const categories = useSelector(state => state.categories)
     const [ text, setText ] = useState('')
-    state= {
-        text: ''
-    }
+    const shopId = props.shop.id
+    // state= {
+    //     text: ''
+    // }
     
-    handleChange = (e) => {
+    // handleChange = (e) => {
       
-    }
+    // }
     
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
         if (text === '') {
             alert('Please write a review before hitting submit')
         } else { 
-        const category = this.props.category
-        const newReview = {...this.state, shop_id: this.props.shop.id}
+        const category = props.category
+        const newReview = {...text, shop_id: shopId}
         console.log("review obj:", newReview)
         dispatch(addReview(newReview, category))
         setText(' ')
+        }
     }
 
     // render() {
-        return (
-            <div className='review-form'>
-                <p className='review-text'>Visited this shop recently? What did you purchase that you love? Leave a review below!</p> 
-            
-                <Form onSubmit={this.handleSubmit}> 
-                    <TextArea id={this.props.shop.id} onChange={e => setText(e.target.value)} value={text} placeholder="The owners here are incredible!" />
-                    <Button color="yellow" type="submit">
-                        Submit
-                    </Button>
-                    <br/>
-                    <br/>
-                </Form>
-            </div>
-        )
-    }
+    return (
+        <div className='review-form'>
+            <p className='review-text'>Visited this shop recently? What did you purchase that you love? Leave a review below!</p> 
+        
+            <Form onSubmit={{handleSubmit}}> 
+                <TextArea id={props.shop.id} onChange={e => setText(e.target.value)} value={text} placeholder="The owners here are incredible!" />
+                <Button color="yellow" type="submit">
+                    Submit
+                </Button>
+                <br/>
+                <br/>
+            </Form>
+        </div>
+    )
+}
+
+export default ReviewForm;
 
 
 // const mapStateToProps = state => { 
@@ -53,4 +57,4 @@ const ReviewForm = () => {
 //         categories: state.categoriesReducer.categories, 
 //     }
 // }
-export default connect(mapStateToProps, {addReview})(ReviewForm)
+// export default connect(mapStateToProps, {addReview})(ReviewForm)
